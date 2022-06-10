4a97e9a7809818859c541992347b0eb10c26f398
Chánh Đỗ <chanhdo@Chanhs-MacBook-Pro.local>
2022-06-10 23:48:34 +0900

    a

--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -4,40 +4,42 @@
 
 
 <div class="container-lg" style="margin: 0 auto;">
-    
+
     <h2 class="text-center mt-2" style="font-family: Georgia, serif; color:blueviolet">Appointments</h2>
 
     <table class="table table-hover">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Department Name</th>
-            <th scope="col">Appointment Date</th>
-            <th scope="col">Taken</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($appointments as $appointment)
-        <tr>
-            <th scope="row">{{$appointment->id}}</th>
-            <td>{{$appointment->department_name}}</td>
-            <td>{{$appointment->appointment_date}}</td>
-            @if($appointment->taken)
-            <td>you can't book this</td>
-            @else
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Department Name</th>
+                <th scope="col">Appointment Date</th>
+                <th scope="col">Taken</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($appointments as $appointment)
+            <tr>
+                <th scope="row">{{$appointment->id}}</th>
+                <td>{{$appointment->department_name}}</td>
+                <td>{{$appointment->appointment_date}}</td>
+                @if($appointment->taken)
+                <td>you can't book this</td>
+                @else
                 <td>
                     <form method="post" action="{{ route('bookAppointment')}}">
                         @csrf
                         <input type="text" style="display:none" value="{{$appointment->id}}" name="appointment_id">
-                        <input type="text" style="display:none" value="{{$appointment->department_name}}" name="department_name">
-                        <input type="text" style="display:none" value="{{$appointment->appointment_date}}" name="appointment_date">
+                        <input type="text" style="display:none" value="{{$appointment->department_name}}"
+                            name="department_name">
+                        <input type="text" style="display:none" value="{{$appointment->appointment_date}}"
+                            name="appointment_date">
                         <input type="submit" value="book" class="btn btn-primary">
                     </form>
                 </td>
-            @endif
-        </tr>
-        @endforeach
-     </tbody>
+                @endif
+            </tr>
+            @endforeach
+        </tbody>
     </table>
 </div>
 
